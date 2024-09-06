ca89829347c86f68da1a523bc7e99659d7f3e6d8
Stan Leey <stanleey0@gmail.com>
2024-09-06 11:52:14 -0400

    modified test js to change frame of work

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 import ttUtilsTest from "./ttUtils.test.js";
 import testUtilsTest from "./testUtils.test.js";
 import chipsimTest from "./chipsim.test.js";
-import chipUtilsTest from "./chipUtils.test.js";
+import matchUtilsTest from "./matchUtils.test.js";
 
-let testSuites = [testUtilsTest, ttUtilsTest, chipUtilsTest, chipsimTest];
+let testSuites = [testUtilsTest, ttUtilsTest, matchUtilsTest, chipsimTest];
 
 testSuites.forEach((suite) => {
   suite.runSuite();
