f264e10434381ae85da85d97d38206717efe1e33
Stan Leey <stanleey0@gmail.com>
2024-09-06 14:26:21 -0400

    Added precededBy and precededByN options

--- matchUtils.js
+++ matchUtils.js
@@ -1,23 +1,38 @@
 /** finds indexes of gates that match a pattern 
  * 
  * @param {object} chip the chip object to match upon
- * @param {Object} param1 options
+ * @param {object} param1 pattern options. we specify which arguments match
  * @param {string} [param1.gate="*"] The gate to match. Can be "AND", "OR", "NOT", or "*" to match all gates. By default it matches all gates.
- * 
+ * @param {string} [param1.precededBy="*"] Checks to see the gate of what the current gate is preceded by. By default it doesn't check for anything.
+ * @param {string | number} [param1.precededByN="*"] Checks to see how many gates precede the current one. By default it accepts all numbers of input.
+ *  
  * @returns {Array} The matches  
 */
-function matchChip(chip, { gate = "*" }) {
+function matchChip(chip, { gate = "*", precededBy = "*", precededByN = "*"}) {
   let matches = [];
 
+  // Checking if the chip is a valid argument
   if (!chip || !Array.isArray(chip.gates)) { // All chips must have gates.
     console.error("matchChip called on a non-chip argument:", chip);
     return []; // No chip, no matches.
   }
 
+  // Checking if we have received a correct precededByN argument
+  if ((precededByN !== "*" && !Number.isInteger(precededByN)) || precededByN < 0) {
+    console.error("matchChip called with bad precededByN argument. Expecting a whole positive number or \"*\", but received:", precededByN);
+    return [];
+  }
+
   // This loop gathers all of the matches.
   chip.gates.forEach((current_gate, i) => {
     // gate star short circuits this conditional. Otherwise it matches the gate.
-    if (gate === "*" || current_gate.gate === gate) {
+    const gateMatches = gate === "*" || current_gate.gate === gate;
+    // We shouldn't even calculate if gateMatches is true because precededBy will not match.
+    // If gateMatches and precededBy is specified to be a particular gate, we search for any gates that precede it.
+    const precededByMatches = gateMatches && (precededBy === "*" || current_gate.input.some(gi => gi >= 0? chip.gates[gi].gate === precededBy : false))
+    // precededByN only matches if the rest match and the number of gates of input match.
+    const precededByNMatches = precededByMatches && (precededByN === "*" || precededByN === current_gate.input.length);
+    if (precededByNMatches) {
       matches.push(i);
     }
   })
--- matchUtils.test.js
+++ matchUtils.test.js
@@ -42,10 +42,13 @@ function test_matchGate() {
     ]
   };
   
-  console.time("matchChip failure");
+  console.time("matchChip argument failure");
   console.info("Testing on no chip. ↓ There should be an error notification below that the chip argument is not a chip.");
-  console.assert(tu.deepArrEq(mu.matchChip(null, "doesn't matter"), []), "match chip called with no chip should return an empty array.")
-  console.timeEnd("matchChip failure");
+  console.assert(tu.deepArrEq(mu.matchChip(null, "doesn't matter"), []), "match chip called with no chip should return an empty array.");
+  console.info("Testing on bad precededByN argument. ↓ There should be 2 error notifications below that the precededByN option is a bad argument");
+  console.assert(tu.deepArrEq(mu.matchChip(gateMap, { precededByN: "This is the first"}), []), "match chip called with precededByN=Some other string should return an empty array.");
+  console.assert(tu.deepArrEq(mu.matchChip(gateMap, { precededByN: -2 }), []), "match chip called with precededByN=-2 should return an empty array.");
+  console.timeEnd("matchChip argument failure");
 
   // Building the set of all gate indices. Helpful for testing.
   let allGatesMatch = [];
@@ -53,7 +56,7 @@ function test_matchGate() {
     allGatesMatch.push(i); // This is so dumb. Why oh why does JS have no range operator?
   }
 
-  console.time("matchChip gate options");
+  console.time("matchChip gate option");
   // Tests all of the gate options. 
   ["NOT", "AND", "OR", "*", undefined].forEach(gateOption => {
     if (gateOption === undefined || gateOption === "*") { // Default and wildcard behavior.
@@ -66,8 +69,20 @@ function test_matchGate() {
       console.assert(tu.deepArrEq(correct, mu.matchChip(gateMap, { gate: gateOption })), "matchChip gate ", gateOption, " is faulty");
       console.timeEnd("matchChip gate options " + gateOption);
     }
-  })
-  console.timeEnd("matchChip gate options");
+  });
+  console.timeEnd("matchChip gate option");
+
+  console.time("matchChip precededBy option");
+  // test the precededBy options
+  console.assert(tu.deepArrEq([2, 3, 6], mu.matchChip(gateMap, {gate: "OR", precededBy: "*"})), "matchChip precededBy * is faulty");
+  console.assert(tu.deepArrEq([6], mu.matchChip(gateMap, {gate: "OR", precededBy: "OR"})), "matchChip precededBy OR is faulty");
+  console.assert(tu.deepArrEq([2, 6], mu.matchChip(gateMap, {gate: "OR", precededBy: "AND"})), "matchChip precededBy AND is faulty");
+  console.timeEnd("matchChip precededBy option");
+  
+  console.time("matchChip precededByN option");
+  // test the precededByN options
+  console.assert(tu.deepArrEq(mu.matchChip(gateMap, {gate: "NOT"}), mu.matchChip(gateMap, {precededByN: 1})), "matchChip precededByN is faulty");
+  console.timeEnd("matchChip precededByN option");
 }
 
 function runSuite() {
