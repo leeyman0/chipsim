74d18d6c86a63a3c1be6e3413ad7575a2e1b27e3
Stan Leey <stanleey0@gmail.com>
2024-10-13 14:26:48 -0400

    implementing deepArrObjEq, a tool I will need to use for matchUtil capture testing

--- src/testUtils.js
+++ src/testUtils.js
@@ -42,6 +42,52 @@ function deepArrEq(a1, a2) {
   else return a1 === a2; // They are both elements and should be compared as elements
 }
 
+/** This is utility function to compare objects and arrays recursively, so that arrays and objects can be handled inside of each other.
+ * Be careful, it doesn't have the ability to handle circular references.
+ *
+ * To check whether the organizations match, it will go through these steps:
+ * 1. Check to see whether o1 and o2 are both arrays.
+ *   - If o1 or o2 but not both are arrays, return false.
+ *   - If they are both arrays, check to see if they are the same length.
+ *     * If they are both arrays of the same length, then use deepArrObjEq to check that every element in o1 matches the corresponding
+ * element in o2.
+ * 2. If they are not arrays, then o1 and o2 are objects or primitives. Check to see whether o1 and o2 are both objects.
+ *   - If o1 or o2 is an object but not both, return false.
+ *   - If they are both objects, then check to see if they have the same keys.
+ *     * If they both have the same keys, then use deepArrObjEq to check that every one of the corresponding values in o1 match o2.
+ * 3. If they are not arrays or objects, use strict equality (`===`) to compare the primitive values.
+ *
+ * @param {*} o1 the first object to compare
+ * @param {*} o2 the second object to compare
+ * @returns {boolean} the result, whether these are practically the same.
+ */
+function deepArrObjEq(o1, o2) {
+  const o1ArrP = Array.isArray(o1);
+  const o2ArrP = Array.isArray(o2);
+  if (o1ArrP !== o2ArrP)
+    return false; // The types don't match
+  else if (o1ArrP && o2ArrP) {
+    if (o1.length !== o2.length) return false;
+    else return o1.every((e, i) => deepArrObjEq(e, o2[i]));
+  } else {
+    // Check to see if they are both objects
+    const o1ObjP = o1.constructor === Object;
+    const o2ObjP = o1.constructor === Object;
+    if (o1ObjP !== o2ObjP) return false;
+    else if (o1ObjP && o2ObjP) {
+      // Check to see if the keys are the same
+      let o1Keys = Object.keys(o1).sort();
+      let o2Keys = Object.keys(o2).sort();
+      if (!deepArrEq(o1Keys, o2Keys)) return false;
+      else return o1Keys.every((k) => deepArrObjEq(o1[k], o2[k]));
+    } else {
+      // They should be compared using strict equality
+      return o1 === o2;
+    }
+  }
+}
+
 export default Object.freeze({
   deepArrEq,
+  deepArrObjEq,
 });
--- test/testUtils.test.js
+++ test/testUtils.test.js
@@ -11,3 +11,28 @@ test("deepArrEq tests deep array equality", () => {
   expect(testUtils.deepArrEq(c, d)).toBeFalsy();
   expect(testUtils.deepArrEq(c, JSON.parse(JSON.stringify(c)))).toBeTruthy();
 });
+
+const e = ["b", { a: 1, b: 2 }],
+  f = ["b", { a: 1, b: 2 }],
+  g = [{ a: 1, b: 2 }, "b"],
+  h = { b: 2, a: 1 },
+  i = { a: 1, b: 2 };
+test("deepArrObjEq tests object and array equality", () => {
+  // First of all, all of the tests of deepArrEq will be true
+  expect(testUtils.deepArrObjEq(a, [...a])).toBeTruthy();
+  expect(testUtils.deepArrObjEq(a, b)).toBeFalsy();
+  expect(testUtils.deepArrObjEq(c, d)).toBeFalsy();
+  expect(testUtils.deepArrObjEq(c, JSON.parse(JSON.stringify(c)))).toBeTruthy();
+  // Secondly, these will also be true
+  expect(testUtils.deepArrObjEq(e, [...e])).toBeTruthy();
+  expect(testUtils.deepArrObjEq(e, f)).toBeTruthy();
+  expect(testUtils.deepArrObjEq(f, g)).toBeFalsy();
+  expect(testUtils.deepArrObjEq(h, i)).toBeTruthy();
+});
+
+const obj1 = { abc: [1, 2, 3], efg: [4, 56], 123: "abc" },
+  obj2 = { efg: [4, 56], abc: [1, 2, 3], 123: "abc" };
+// const obj3, obj4;
+test("deepArrObjEq tests nested object and arrays recursively", () => {
+  expect(testUtils.deepArrObjEq(obj1, obj2)).toBeTruthy();
+});
